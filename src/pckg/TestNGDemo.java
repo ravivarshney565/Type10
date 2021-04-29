1ad1fdd1a652319133ff79475706497c2193bea0
ravivarshney565 <ravivarshney@QAIT.COM>
2021-04-29 23:20:49 +0530

    adding files

---
+++ src/pckg/TestNGDemo.java
@@ -0,0 +1,15 @@
+package pckg;
+import org.testng.annotations.Test;
+
+public class TestNGDemo {
+	
+    @Test
+	public void testLoginWithValidCredentials()
+	
+	{
+		System.out.println("Inside Testng - @Test - testLoginWithValidCredntials");
+		
+		 int a=10/0;
+	}
+
+}
